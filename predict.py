36803c2a0d1319f830ed9fb3294f1214a9d86a08
bxb370 <Brooke.Brocker@sherwin.com>
2026-05-27 08:55:05 -0400

    added function structure for the api

--- predict.py
+++ predict.py
@@ -1,1 +1,11 @@
 """this is for later when we need to make the API"""
+
+
+def predict_image(image_bytes):
+    # 1. load image
+    # 2. preprocess
+    # 3. extract features
+    # 4. run model
+    # 5. format output
+    
+    return result
